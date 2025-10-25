ad1af1da641b1242bc15aa2783da3e8de9b51e63
Marcky Paulo Balaba <marckybalabamlacc@gmail.com>
2025-10-25 18:49:58 +0800

    Add files via upload

---
+++ main_package/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main_package;
+import process_package.*;
+/**
+ *
+ * @author paulo
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+       
+        TuitionManager start = new TuitionManager();
+        
+        start.startingPoint();
+        
+    }
+    
+}
